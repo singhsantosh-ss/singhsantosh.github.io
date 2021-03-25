196d60b1e2010d6e638141ac744f767b4f0c8493
santosh singh <singhsantosh.edu.in@gmail.com>
2021-03-25 11:24:32 +0530

    initial set profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {MatDialog} from '@angular/material/dialog';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(public dialog: MatDialog
+    ) {}
+
+  openDialog() {
+    this.dialog.open(AppComponent);
+  }
 }
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me/about-me.component.html',
+  styleUrls: ['./about-me/about-me.component.css']
+})
+export class AboutMeComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatButtonModule, MatCheckboxModule, MatDatepickerModule, 
 MatFormFieldModule, MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule, 
-  MatSlideToggleModule
+  MatSlideToggleModule, MatBadgeModule, MatBottomSheetModule, MatButtonToggleModule, MatCardModule, MatChipsModule,
+    MatStepperModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule,
+    MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule,MatProgressBarModule,
+  MatProgressSpinnerModule, MatRippleModule, MatSidenavModule, MatSnackBarModule, MatSortModule,
+  MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule, MatCommonModule
 } from '@angular/material'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PortalModule } from '@angular/cdk/portal';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
@@ -29,8 +42,45 @@ import { AppComponent } from './app.component';
     MatSlideToggleModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatBadgeModule,
+    MatBottomSheetModule, 
+    MatButtonToggleModule,
+    MatCardModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRippleModule,
+    MatCommonModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    PortalModule,
+    ScrollingModule,
   ],
+  entryComponents: [
+    AboutMeComponent,
+  ],
+  exports: [ AboutMeComponent ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
